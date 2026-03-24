828c1586b8391239e773e08d61444a8b1dee9533
Emma <bemmab03@gmail.com>
2026-03-24 19:21:44 +0100

    Emmas contribution

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Main Version");
         System.out.println("This is a branch!");
+        System.out.println("Emma was here!");
     }
     
 }
