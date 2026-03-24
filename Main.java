542bc07a2be9a8b2cfaf8d390466fb0b52fc0c6a
leandra-k <leandraknappe@MacBook-Air-von-leandra.local>
2026-03-24 18:43:20 +0100

    branch message

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.net.SocketPermission;
+
 public class Main { 
     public static void main(String[] args) {
         System.out.println("Main Version");
+        System.out.println("This is a branch!");
     }
     
 }
